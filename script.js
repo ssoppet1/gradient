92174f9e2a0d524bdf017b0c5c326588f3303361
Stacy <105446539+ssoppet1@users.noreply.github.com>
2022-06-21 08:22:32 -0500

    Update script.js
    added function

--- script.js
+++ script.js
@@ -3,16 +3,20 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
+function setGradient() {
+	body.style.background =
+		"linear-gradient(to right, "
+	+ color1.value
+	+ ", "
+	+ color2.value
+	+ ")";
+
+}
 
 
 
+color1.addEventListener("input", setGradient);
 
 
-color1.addEventListener("input", function() {
-	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ");"
 
-})
-
-color2.addEventListener("input", function() {
-	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ");"
-})
+color2.addEventListener("input", setGradient);
